328e0627a346a245594a6f52197ba76152ed76c6
vignesh1999 <vinu6th@gmail.com>
2021-08-02 15:51:32 +0530

    changed auth

--- src/features/Auth/Login.js
+++ src/features/Auth/Login.js
@@ -1,89 +1,89 @@
-import "./login.css";
-import { Link, useNavigate, useLocation } from "react-router-dom";
-import { RiLoginBoxFill } from "react-icons/ri";
-import { useState } from "react";
-import { loginAsync } from "./auth.service";
-import { useDispatch, useSelector } from "react-redux";
-import { selectAuth } from "../Auth/authSlice";
-import { Loader } from "../../components/Loader/Loader";
+import './login.css'
+import { Link, useNavigate, useLocation } from 'react-router-dom'
+import { RiLoginBoxFill } from 'react-icons/ri'
+import { useState } from 'react'
+import { loginAsync } from './auth.service'
+import { useDispatch, useSelector } from 'react-redux'
+import { selectAuth } from '../Auth/authSlice'
+import { Loader } from '../../components/Loader/Loader'
 export default function Login() {
-  let from = "";
-  const navigate = useNavigate();
-  const { state } = useLocation();
-  from = state?.from;
+  let from = ''
+  const navigate = useNavigate()
+  const { state } = useLocation()
+  from = state?.from
   const [userData, setUserData] = useState({
-    email: "",
-    password: ""
-  });
-  const dispatch = useDispatch();
-  const auth = useSelector(selectAuth);
-  const authStatus = useSelector((state) => state.auth.status);
-  const error = useSelector((state) => state.auth.error);
+    email: '',
+    password: '',
+  })
+  const dispatch = useDispatch()
+  const auth = useSelector(selectAuth)
+  const authStatus = useSelector((state) => state.auth.status)
+  const error = useSelector((state) => state.auth.error)
 
   const submit = async (e) => {
-    e.preventDefault();
+    e.preventDefault()
     const response = await dispatch(
       loginAsync({
-        email: userData["email"],
-        password: userData["password"]
+        email: userData['email'],
+        password: userData['password'],
       })
-    );
+    )
     if (response.payload?.success) {
-      navigate(from || "/");
+      navigate(from || '/')
     }
-  };
+  }
 
   function onChangeHandler(e) {
     setUserData({
       ...userData,
-      [e.currentTarget.id]: JSON.parse(JSON.stringify(e.currentTarget.value))
-    });
+      [e.target.id]: JSON.parse(JSON.stringify(e.target.value)),
+    })
   }
 
   return (
     <div>
-      {authStatus === "loading" && <Loader />}
-      {(authStatus === "idle" || authStatus === "failed") && (
-        <div className="auth-container">
+      {authStatus === 'loading' && <Loader />}
+      {(authStatus === 'idle' || authStatus === 'failed') && (
+        <div className='auth-container'>
           <form
-            className="auth-form"
+            className='auth-form'
             onSubmit={submit}
-            style={{ color: "black", fontSize: "15px" }}
+            style={{ color: 'black', fontSize: '15px' }}
           >
-            <div style={{ margin: "auto", color: "red", marginBottom: "1rem" }}>
-              {authStatus === "failed"
-                ? error === "Request failed with status code 401"
-                  ? "Username or Password Wrong"
-                  : "Please Signup"
-                : ""}
+            <div style={{ margin: 'auto', color: 'red', marginBottom: '1rem' }}>
+              {authStatus === 'failed'
+                ? error === 'Request failed with status code 401'
+                  ? 'Username or Password Wrong'
+                  : 'Please Signup'
+                : ''}
             </div>
             <h3>Login</h3>
             <label>Email</label>
             <input
-              id="email"
-              type="email"
+              id='email'
+              type='email'
               value={userData.email}
               required
-              placeholder="Enter Email"
-              autoComplete="off"
+              placeholder='Enter Email'
+              autoComplete='off'
               onChange={onChangeHandler}
             />
             <label>Password</label>
             <input
-              id="password"
+              id='password'
               value={userData.password}
-              type="password"
+              type='password'
               required
-              placeholder="Enter Password"
+              placeholder='Enter Password'
               onChange={onChangeHandler}
             />
-            <button type="submit" className="link_btn">
+            <button type='submit' className='link_btn'>
               <RiLoginBoxFill />
-              {authStatus === "loading" ? "loading" : "Login"}
+              {authStatus === 'loading' ? 'loading' : 'Login'}
             </button>
-            <div className="signup">
-              Not a user?{" "}
-              <Link to="/signup" className="primary_link">
+            <div className='signup'>
+              Not a user?{' '}
+              <Link to='/signup' className='primary_link'>
                 Signup
               </Link>
             </div>
@@ -91,5 +91,5 @@ export default function Login() {
         </div>
       )}
     </div>
-  );
+  )
 }
--- src/features/Feed/Tweets.js
+++ src/features/Feed/Tweets.js
@@ -1,109 +1,110 @@
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faComment, faHeart as full } from "@fortawesome/free-solid-svg-icons";
-import { useEffect, useState } from "react";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faComment, faHeart as full } from '@fortawesome/free-solid-svg-icons'
+import { useEffect, useState } from 'react'
 import {
   postTweetAsync,
   getFeedAsync,
   likeButtonPressed,
-  incrementLike
-} from "./feedSlice";
-import { useDispatch, useSelector } from "react-redux";
-import { selectFeed } from "../Feed/feedSlice";
-import { Loader } from "../../components/Loader/Loader";
+  incrementLike,
+} from './feedSlice'
+import { useDispatch, useSelector } from 'react-redux'
+import { selectFeed } from '../Feed/feedSlice'
+import { Loader } from '../../components/Loader/Loader'
 
-import { faHeart } from "@fortawesome/free-regular-svg-icons";
-import "./Tweets.css";
+import { faHeart } from '@fortawesome/free-regular-svg-icons'
+import './Tweets.css'
 export default function Tweets() {
-  const [text, setText] = useState("");
-  const [tweet1, setTweet] = useState(0);
-  const dispatch = useDispatch();
-  const [tweets, setTweets] = useState([]);
-  const feed = useSelector(selectFeed);
-  const feedStatus = useSelector((state) => state.feed.status);
-  const feedTweets = useSelector((state) => state.feed.tweets);
-  const feedUser = useSelector((state) => state.feed.user);
+  const [text, setText] = useState('')
+  const [tweet1, setTweet] = useState(0)
+  const dispatch = useDispatch()
+  const [tweets, setTweets] = useState([])
+  const feed = useSelector(selectFeed)
+  const feedStatus = useSelector((state) => state.feed.status)
+  const feedTweets = useSelector((state) => state.feed.tweets)
+  const feedUser = useSelector((state) => state.feed.user)
 
   useEffect(() => {
-    if (feedStatus === "idle") dispatch(getFeedAsync());
-    setTweets(feedTweets);
-  }, [feedStatus]);
+    if (feedStatus === 'idle' || feedStatus === 'loading')
+      dispatch(getFeedAsync())
+    setTweets(feedTweets)
+  }, [feedStatus])
 
   useEffect(() => {
-    setTweet(!tweet1);
-  }, [tweets]);
+    setTweet(!tweet1)
+  }, [tweets])
 
   function textHandler(e) {
-    const fileList = e.target.value;
-    setText(fileList);
+    const fileList = e.target.value
+    setText(fileList)
   }
 
   async function handleClick(text) {
-    setTweet(1);
+    setTweet(1)
     if (text.length > 0) {
-      await dispatch(postTweetAsync({ text }));
+      await dispatch(postTweetAsync({ text }))
     }
   }
 
   return (
-    <div className="centre-box" style={{ marginBottom: "10rem" }}>
-      <div className="container-home">
-        <div className="text-home">Home</div>
+    <div className='centre-box' style={{ marginBottom: '10rem' }}>
+      <div className='container-home'>
+        <div className='text-home'>Home</div>
       </div>
-      {feedStatus === "loading" && <Loader />}
-      {feedStatus === "success" && (
+      {feedStatus === 'loading' && <Loader />}
+      {feedStatus === 'success' && (
         <div>
-          <div className="happenning">
-            <div className="happenning-btn">
-              <img className="profile-btn" src={feedUser.profileImg} alt="" />
+          <div className='happenning'>
+            <div className='happenning-btn'>
+              <img className='profile-btn' src={feedUser.profileImg} alt='' />
             </div>
             <div>
               <input
-                type="text"
+                type='text'
                 placeholder="What's happenning"
-                className="primary-txt"
+                className='primary-txt'
                 onChange={textHandler}
               />
             </div>
-            <div class="post-btn-wrapper">
+            <div class='post-btn-wrapper'>
               <button
-                className="post-btn"
+                className='post-btn'
                 onClick={() => {
-                  handleClick(text);
+                  handleClick(text)
                 }}
               >
                 Post
               </button>
             </div>
           </div>
-          <div className="blank"></div>
+          <div className='blank'></div>
           {feedTweets?.map(({ _id, user, content, contentImg, likes }, i) => {
             return (
-              <div className="tweet" key={_id}>
-                <div className="tweet-container">
-                  <div className="happenning-btn">
-                    <img className="profile-btn" src={user.profileImg} alt="" />
+              <div className='tweet' key={_id}>
+                <div className='tweet-container'>
+                  <div className='happenning-btn'>
+                    <img className='profile-btn' src={user.profileImg} alt='' />
                   </div>
-                  <div className="tweet-text">
-                    <span className="bold">{user.name} </span>
-                    <span className="diff-color">@{user.tag}</span>
+                  <div className='tweet-text'>
+                    <span className='bold'>{user.name} </span>
+                    <span className='diff-color'>@{user.tag}</span>
                     <br />
-                    <div className="less-font">{content}</div>
+                    <div className='less-font'>{content}</div>
                     {contentImg && (
                       <div>
-                        <img src={contentImg} className="img-small" alt="" />
+                        <img src={contentImg} className='img-small' alt='' />
                       </div>
                     )}
                   </div>
                 </div>
-                <div className="icons-container">
+                <div className='icons-container'>
                   <ul>
                     <li>
                       <FontAwesomeIcon
-                        className="small-icon"
+                        className='small-icon'
                         icon={faHeart}
                         onClick={() => {
-                          dispatch(likeButtonPressed(_id));
-                          dispatch(incrementLike(_id));
+                          dispatch(likeButtonPressed(_id))
+                          dispatch(incrementLike(_id))
                         }}
                       />
                       {likes}
@@ -118,10 +119,10 @@ export default function Tweets() {
                   </ul>
                 </div>
               </div>
-            );
+            )
           })}
         </div>
       )}
     </div>
-  );
+  )
 }
